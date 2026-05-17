a7d814d4b006e224399181dcdf39a31a1acd9e67
Aayush Waghade <waghadeaayush@gmail.com>
2026-05-17 20:55:31 +0530

    Switched to WhatsApp Cloud API

--- whatsapp_sender.py
+++ whatsapp_sender.py
@@ -1,32 +1,27 @@
-# whatsapp_sender.py
+import requests
+import os
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-import time
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
+ACCESS_TOKEN = os.getenv("WHATSAPP_TOKEN")
 
-phone_number = "919404319706"
-message = "🔥 AI Meeting Guardian Activated Successfully"
+def send_whatsapp_message(message):
 
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install())
-)
+    url = f"https://graph.facebook.com/v22.0/{PHONE_NUMBER_ID}/messages"
 
-driver.get(f"https://web.whatsapp.com/send?phone={phone_number}&text={message}")
+    headers = {
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Content-Type": "application/json"
+    }
 
-print("Scan QR Code if needed...")
-time.sleep(25)
+    data = {
+        "messaging_product": "whatsapp",
+        "to": "919404319706",
+        "type": "text",
+        "text": {
+            "body": message
+        }
+    }
 
-message_box = driver.find_element(
-    By.XPATH,
-    '//div[@contenteditable="true"][@data-tab="10"]'
-)
+    response = requests.post(url, headers=headers, json=data)
 
-message_box.send_keys(Keys.ENTER)
-
-print("Message Sent Successfully ✅")
-
-time.sleep(10)
-driver.quit()
+    print(response.text)
